b1ed1fa67d92b56ac28d455cdffeca39b028eee9
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-10-02 19:38:09 +0200

    Delete checkID.h

--- checkID.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-#include <fstream>
-
-/**@brief
-	*Fukcja sprawdza czy wprowadzony pesel jest poprawny.
-	*/
-bool checkID(int IDnumber, std::vector<std::string> bazaVector, std::string name_, std::string lastName_, std::string homeCountry_, std::string cityOfResidence_, std::string streetName_, std::string numberOfbulding_, int dayOfBirth_, int monthOfBirth_, int yearOfBirth_);
