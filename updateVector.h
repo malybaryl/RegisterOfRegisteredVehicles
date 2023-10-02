7a487857dc12dbf14b5a47fdcd5a0331e1b5a04e
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-10-02 19:36:10 +0200

    Delete updateVector.h

--- updateVector.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <iostream>
-#include <fstream>
-
-/**@brief
-*Funkcja czyta plik baza.txt i zapisuje go do vektora stringów bazaVector
-*/
-void updateVector(std::vector <std::string>& bazaVector);
